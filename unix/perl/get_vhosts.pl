64d282c75999205be024106bc4a75b9800c34e88
adrian <adrian@slacknet.ca>
2011-10-05 10:56:45 -0400

    added header to get_vhosts.pl to describe purpose/version/os

--- unix/perl/get_vhosts.pl
+++ unix/perl/get_vhosts.pl
@@ -1,19 +1,19 @@
 #!/usr/bin/env perl
 
-# written for freebsd, may need some modification for other platforms
+# os: freebsd
+# version: 1.0
+# purpose: returns all interface IP{v4,v6} addresses and associated PTR records
+
 
 #ifconfig | awk '$1 ~ /inet[0-9]*/ && $2 !~ /fe80::1%lo0|::1|127.0.0.1/ {print $2};' | xargs -n 1 host | awk '$5 ~ /.*[.]$/ {print $5}'
-
 my @v4 = `ifconfig | awk '\$1 ~ /inet\$/ && \$2 !~ /fe80::1%lo0|::1|127.0.0.1/ {print \$2};'`;
 my @v6 = `ifconfig | awk '\$1 ~ /inet6\$/ && \$2 !~ /fe80::1%lo0|::1|127.0.0.1/ {print \$2};'`;
 
 chomp @v4; chomp @v6;
 
-
 print <<HEADER;
 We are always adding vhosts! Check back soon!
 HEADER
-
 
 print "\nIPv4 Vhosts\n-----------\n\n";
 for $x (@v4) {
